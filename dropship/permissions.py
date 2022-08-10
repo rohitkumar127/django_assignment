af44ae0a6c1be58b38073149e382b132b2bd079b
Kirankumar_ Mistry Sanketkumar (mikirankumar@deloitte.com) <mikirankumar@your.hostname.com>
2022-08-10 18:10:34 +0530

    Views.py Updated

--- dropship/permissions.py
+++ dropship/permissions.py
@@ -6,7 +6,7 @@ class IsProjectManager(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method == 'GET':
             return True
-        if request.user and request.user.groups.filter(name="IsProjectManager"):
+        if request.user and request.user.groups.filter(name="IsProjectManager") or request.user.is_staff:
             return True
         return False
 
@@ -15,6 +15,6 @@ class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method == 'GET':
             return True
-        if request.user and request.user.groups.filter(name="IsAdmin") or request.user:
+        if request.user and request.user.groups.filter(name="IsAdmin") or request.user.is_staff:
             return True
         return False
